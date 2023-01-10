a56da23e2720831fd0e02145758f5864b2d2333d
chunga <chunga7879@gmail.com>
2023-01-09 23:55:35 -0800

    document ing

--- src/pages/Document.tsx
+++ src/pages/Document.tsx
@@ -1,8 +1,14 @@
 import home1 from "../images/home1.png";
 import React from "../images/React.png";
-import { BiMenu } from 'react-icons/bi';
+import { BiMenu } from "react-icons/bi";
+import { AiFillProject } from "react-icons/ai";
+import { IoIosSchool } from "react-icons/io";
 
 export default function Document() {
+    const onClickDownload = (path: string) => {
+
+    }
+
     return (
         <div className="document">
             <div className="s1"></div>
@@ -10,10 +16,18 @@ export default function Document() {
                 <img src={home1} alt="computer" className="back" />
                 <div className="inner">
                     <div className="subScene">
-                        <BiMenu>Resume</BiMenu>
-                        <>Portfolio</>
-                        <>Transcript</>
-
+                        <div className="doc">
+                            <a href="../pdf/resume_chunga_lee.pdf" download="resume"><BiMenu className="file"/></a>
+                            <h2>Resume</h2>
+                        </div>
+                        <div className="doc">
+                            <a href="/" download="portfolio"><AiFillProject className="file" /></a>
+                            <h2>Portfolio</h2>
+                        </div>
+                        <div className="doc">
+                            <a href="../pdf/transcript_chunga_lee.pdf" download="transcript"><IoIosSchool className="file" /></a>
+                            <h2>Transcript</h2>
+                        </div>
                     </div>
                 </div>
             </div>

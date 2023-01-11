0abed6dc76aeb9277577277de0c4f25bfd561bb1
chunga <chunga7879@gmail.com>
2023-01-11 02:29:35 -0800

    fix head part

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,27 @@ import {Link} from "react-router-dom";
 export function Header() {
     return (
         <header className="app-header">
-            <a href="/" className="logo">
-                <img src={logo} className="img" alt="logo" />
-            </a>
-            <div className="menu-wrap">
-                <ul className="menu">
-                    <li>
-                        <Link to="./education" className="each">Education</Link>
-                    </li>
-                    <li>
-                        <Link to="./projects/1" className="each">Project</Link>
-                    </li>
-                    <li>
-                        <Link to="./experience" className="each">Experience</Link>
-                    </li>
-                    <li>
-                        <Link to="./documents" className="each">Document</Link>
-                    </li>
-                </ul>
+            <div className="wrapper">
+                <a href="/" className="logo">
+                    <img src={logo} className="img" alt="logo" />
+                </a>
+                <div className="menu-wrap">
+                    <ul className="menu">
+                        <li>
+                            <Link to="./education" className="each">Education</Link>
+                        </li>
+                        <li>
+                            <Link to="./projects/1" className="each">Project</Link>
+                        </li>
+                        <li>
+                            <Link to="./experience" className="each">Experience</Link>
+                        </li>
+                        <li>
+                            <Link to="./documents" className="each">Document</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
         </header>
     );
 }
